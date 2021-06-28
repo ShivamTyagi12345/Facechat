1e9ffbbf6a729ef395cff5a62e1bfd295c86f715
shivam tyagi <60812924+ShivamTyagi12345@users.noreply.github.com>
2021-06-28 23:59:38 +0530

    .

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            Video Chat APP
+        </div>
+    )
+}
+
+export default App
--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const app = require("express")();
 const server = require("http").createServer(app);
 const cors = require("cors");
 const io = require("socket.io")(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    }
+	cors: {
+		origin: "*",
+		methods: ["GET", "POST"]
+	}
 });
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
-    res.send('Server is Running.');
+	res.send('Server is Running.');
 });
 
 io.on("connection", (socket) => {
