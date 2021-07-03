8b2f2678bb70bc43eeddb6564213132ee370f2b1
shivam tyagi <60812924+ShivamTyagi12345@users.noreply.github.com>
2021-07-03 18:12:18 +0530

    hgfu

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Typography,AppBar } from '@material-ui/core';
+import Options from "./components/Options";
+import Notifications from './components/Notifications';
+import Video from "./components/Video";
 
+import {makeStyles} from '@material-ui/core/styles';
+import options from "./components/Options";
+
+
+
+const useStyles = makeStyles((theme) => ({
+    appBar: {
+      borderRadius: 15,
+      margin: '30px 100px',
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: '600px',
+      border: '2px solid black',
+  
+      [theme.breakpoints.down('xs')]: {
+        width: '90%',
+      },
+    },
+    image: {
+      marginLeft: '15px',
+    },
+    wrapper: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      width: '100%',
+    },
+  }));
 const App = () => {
+    const classes =useStyles();
     return (
-        <div>
-            Video Chat APP
+        <div  className={classes.wrapper}>
+            <AppBar className={classes.appBar} position ="static" color="inherit">
+                <Typography variant="h2" align ="center">
+                    Video Chat
+                </Typography>
+            </AppBar>
+            < Video/>
+            <options>
+                <Notifications />
+            </options>
         </div>
     )
 }
---
+++ client/src/components/Options.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const options = (children) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default options
